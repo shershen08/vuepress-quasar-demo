60e85b0b764028a09385a707ba58f3b50e2e1235
M Kuznetcov <michail.kuznetsov@gmail.com>
2023-07-26 19:21:23 +0300

    adjust homepage

--- docs/.vuepress/components/HelloWorld.vue
+++ docs/.vuepress/components/HelloWorld.vue
@@ -1,7 +1,8 @@
 <template>
-    <h1>HelloWorld</h1>
-    <button @click="onClick">{{ text }}</button>
-    <template v-if="show">{{ text }}</template>
+    <h1>Hello World App</h1>
+    <p>Azure SSO login redirect page for Hivetest</p>
+    <!-- <button @click="onClick">{{ text }}</button>
+    <template v-if="show">{{ text }}</template> -->
 </template>
 <script lang="ts" setup>
 import {ref} from 'vue'
